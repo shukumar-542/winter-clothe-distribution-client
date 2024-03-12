0beacf86b96db9c98ae8504f01f78a823a106062
Shukumar Ghosh <shukumar542@gamil.com>
2024-03-12 14:24:58 +0600

    added backend deployed url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/v1' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://l2-b2-assignment-7-backend-shukumar-542.vercel.app/api/v1' }),
     tagTypes : ['products' , 'comment','volunteers','donors'],
     endpoints: () => ({})
 })
